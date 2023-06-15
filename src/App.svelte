4c2bc98affcafa5d64c10898290fb6a03c435ff0
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-14 23:18:15 -0600

    bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,36 @@
 <script>
+  let users = [];
+
   const loadUsers = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
     console.log(data);
+    users = data;
   };
 
   loadUsers();
 </script>
 
 <main>
-  <h1>Hello world</h1>
+  <div class="container">
+    <div class="row">
+      <h1>Hello world</h1>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {#each users as user}
+            <tr>
+              <td>{user.name}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
 </main>
 
 <style>
